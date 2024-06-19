402bb5cd09629bbf67f657a69673897dd9bcc49d
Ilya Liburkin <106841014+ColdGold1@users.noreply.github.com>
2024-06-19 19:51:33 +0300

    Add files via upload

--- src/task2/Generator.java
+++ src/task2/Generator.java
@@ -7,13 +7,37 @@ import java.util.Random;
 public class Generator {
     public static void main(String[] args) {
         Random r = new Random();
-        try(FileWriter writer = new FileWriter("src/task2/file1.txt")){
-           for (int i = 0; i < 3000000; i++){
-               StringBuilder sb = new StringBuilder();
-               sb.append(r.nextInt(255)).append('.').append(r.nextInt(255)).append('.')
-                       .append(r.nextInt(255)).append('.').append(r.nextInt(255));
-               writer.write(sb.toString() + "\n");
-           }
+        try (FileWriter writer = new FileWriter("src/task2/file1.txt")) {
+            for (long i = 0; i < 10; i++) {
+                StringBuilder sb = new StringBuilder();
+                sb.append(r.nextInt(255)).append('.').append(r.nextInt(255)).append('.')
+                        .append(r.nextInt(255)).append('.').append(r.nextInt(255));
+                writer.write(sb.toString() + "\n");
+            }
+            /*for (long i = 0; i < 900000000; i++) {
+                StringBuilder sb = new StringBuilder();
+                sb.append(r.nextInt(255)).append('.').append(r.nextInt(255)).append('.')
+                        .append(r.nextInt(255)).append('.').append(r.nextInt(255));
+                writer.write(sb.toString() + "\n");
+            }
+            for (long i = 0; i < 900000000; i++) {
+                StringBuilder sb = new StringBuilder();
+                sb.append(r.nextInt(255)).append('.').append(r.nextInt(255)).append('.')
+                        .append(r.nextInt(255)).append('.').append(r.nextInt(255));
+                writer.write(sb.toString() + "\n");
+            }
+            for (long i = 0; i < 900000000; i++) {
+                StringBuilder sb = new StringBuilder();
+                sb.append(r.nextInt(255)).append('.').append(r.nextInt(255)).append('.')
+                        .append(r.nextInt(255)).append('.').append(r.nextInt(255));
+                writer.write(sb.toString() + "\n");
+            }
+            for (long i = 0; i < 900000000; i++) {
+                StringBuilder sb = new StringBuilder();
+                sb.append(r.nextInt(255)).append('.').append(r.nextInt(255)).append('.')
+                        .append(r.nextInt(255)).append('.').append(r.nextInt(255));
+                writer.write(sb.toString() + "\n");
+            }*/
         } catch (IOException e) {
             e.printStackTrace();
         }
--- src/task2/RunnerTask2.java
+++ src/task2/RunnerTask2.java
@@ -4,22 +4,43 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.BitSet;
 import java.util.HashSet;
 
 public class RunnerTask2 {
-    public static void main(String[] args) {
+    private static final int HALF_IP = (int) ((1L << 31) - 1);
 
-        HashSet<String> setOfIPv4 = new HashSet<String>();
+    public static void main(String[] args) {
+        System.out.println("total memory = " + Runtime.getRuntime().totalMemory());
+        //HashSet<String> setOfIPv4 = new HashSet<String>();
+        BitSet bs1 = new BitSet(HALF_IP);
+        BitSet bs2 = new BitSet(HALF_IP);
         try (BufferedReader br = new BufferedReader(new FileReader("src/task2/file1.txt"))) {
             String s;
             while ((s = br.readLine()) != null) {
-                boolean b = setOfIPv4.add(s);
+                long ip = ipToInt(s);
+                if (ip > HALF_IP) {
+                    bs2.set((int) (ip - HALF_IP));
+                } else {
+                    bs1.set((int) (ip));
+                }
             }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.println(setOfIPv4.size());
+        long result = (long) bs1.cardinality() + (long) bs2.cardinality();
+        //System.out.println("used memory = " + (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()));
+        System.out.println("cardinality = " + result);
+    }
+
+    private static long ipToInt(String s) {
+        String[] s1 = s.split("\\.");
+        long result = 0;
+        for (int i = 0; i < s1.length; i++) {
+            long k = Integer.parseInt(s1[i]);
+            result = (result << 8) + k;
+        }
+        return result;
     }
 }
